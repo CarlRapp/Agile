04878ce4a2d4b8c3cc7b993a428c417ad630b645
Erik <nil_erik@hotmail.com>
2014-09-04 13:57:09 +0200

    ..

---
+++ Breakout/Breakout/Graphics/GraphicsManager.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef GRAPHICSMANAGER_H
+#define GRAPHICSMANAGER_H
+
+
+class GraphicsManager
+{
+
+public:
+
+	//GraphicsManager(ID3D11Device *device, ID3D11DeviceContext *deviceContext, ID3D11RenderTargetView *renderTargetView, int width, int height);
+
+};
+
+
+
+#endif
