2ea272a9285f7e9c66f6993af04e9cb698f72d34
Linux-Dev <>
2014-09-15 12:46:27 +0200

    Fixed input for Linux

--- Breakout/Breakout/Audio/LinAudio.cpp
+++ Breakout/Breakout/Audio/LinAudio.cpp
@@ -1,6 +1,6 @@
 #include "LinAudio.h"
 
-LinAudio::LinAudio()
+LinAudio::LinAudio() 
 : IAudio()
 {
 }
@@ -15,15 +15,65 @@ LinAudio::~LinAudio()
 
 bool LinAudio::Initialize()
 {
+
+	if(SDL_Init(SDL_INIT_AUDIO) < 0)
+	{
+		//Log(MSG_ERROR, "SDL_Audio:", SDL_GetError());
+		return false;
+	}
+
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
+	{
+		//Log(MSG_ERROR, "SDL_Audio:", SDL_GetError());
+		return false;
+	}
+
+
+
 	return true;
 }
 
 bool LinAudio::PlayMusic(const char* _fileName, int _loop)
 {
+	Mix_Music* music = FileManager::GetInstance().LoadMusic(_fileName);
+	if (!music)
+	{
+		//Log(MSG_ERROR, "SDL_Audio, Music not found:", _fileName);
+		return false;
+	}
+
+	// Another music already playing, switching
+	//if (Mix_PlayingMusic() != 0)
+	//	Log(MSG_WARNING, "SDL_Audio: Already playing track. Changing to new."); // Could possible be removed
+
+	// Play the music track
+	if (Mix_PlayMusic(music, _loop) == -1)
+	{
+		//Log(MSG_ERROR, "SDL_Audio:", Mix_GetError());
+		return false;
+	}
+
 	return true;
 }
 
+
 bool LinAudio::PlaySoundEffect(const char* _fileName, int _loop)
 {
+	// Sound not found, return false
+	Mix_Chunk* soundEffect = 0;// = FileManager::GetInstance().LoadSoundEffect(_fileName);
+	if (!soundEffect)
+	{
+		//Log(MSG_ERROR, "SDL_Audio, Sound effect not found:", _fileName);
+		return false;
+	}
+
+	// Play the sound
+	if (Mix_PlayChannel(-1, soundEffect, _loop) == -1)
+	{
+		//Log(MSG_ERROR, "SDL_Audio:", Mix_GetError());
+		return false;
+	}
+
 	return true;
+
 }
--- Breakout/Breakout/Graphics/OpenGL/GLGraphics.h
+++ Breakout/Breakout/Graphics/OpenGL/GLGraphics.h
@@ -12,8 +12,8 @@
 #include <SDL/SDL.h>
 
 #include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
+//#include <glm/gtc/matrix_transform.hpp>
+//#include <glm/gtc/type_ptr.hpp>
 
 #include <vector>
 //#include <string>
