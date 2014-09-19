6ce55a23639c137cf69070bba29b5e2a909bfb65
ThePettsoN <pontaren_nilsson@hotmail.com>
2014-09-19 13:25:33 +0200

    Merge fix

--- Breakout/Breakout/Graphics/ICamera.h
+++ Breakout/Breakout/Graphics/ICamera.h
@@ -22,8 +22,8 @@ public:
 	//virtual void RotateY(float angle) = 0;
 
 #pragma region Get Functions
-	virtual MATRIX4* GetView() = 0;
-	virtual MATRIX4* GetProjection() = 0;
+	virtual void* GetView() = 0;
+	virtual void* GetProjection() = 0;
 
 	virtual VECTOR3 GetPosition() = 0;
 	//virtual Vector3 GetForward() = 0;
