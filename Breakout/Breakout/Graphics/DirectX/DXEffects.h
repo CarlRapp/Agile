cc9aa9cf25d5390821ad8c4ebb44b576c5b05155
Erik <nil_erik@hotmail.com>
2014-10-15 13:00:43 +0200

    Release stuff

--- Breakout/Breakout/Graphics/DirectX/DXEffects.h
+++ Breakout/Breakout/Graphics/DirectX/DXEffects.h
@@ -19,7 +19,12 @@
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "d3dcompiler.lib")
 
+#ifdef _DEBUG
 #pragma comment(lib, "Effects11D.lib")
+#else
+#pragma comment(lib, "Effects11.lib")
+#endif
+
 
 #pragma region DXEffect
 class DXEffect
